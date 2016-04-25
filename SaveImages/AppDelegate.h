627c3f2d57d831fccc5330e368a4542ff4f65ff5
GhostKZShadow <GhostKZShadow@163.com>
2016-04-25 17:34:32 +0800

    Initial Commit

---
+++ SaveImages/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SaveImages
+//
+//  Created by 张坤 on 16/4/25.
+//  Copyright © 2016年 KZ. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
